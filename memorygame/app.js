fdc450918b3a32fd2a510bba8d7aa37fcc2f08cc
gitavk <3apa3ka3@gmail.com>
2022-03-10 22:16:32 +0100

    Memory game is done.

--- memorygame/app.js
+++ memorygame/app.js
@@ -28,14 +28,44 @@ let cardArray = [
   cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
+const cardChosen = new Set([])
+const cardChosenIds = new Set([])
+const boardCards = []
 
 function createBoard() {
- for (let i = 0; i < 10; i++) {
+ for (let i = 0; i < cardArray.length; i++) {
     const card = document.createElement('img')
     card.setAttribute('src', 'images/blank.png')
-    card.setAttribute('id', i)
+    card.setAttribute('data-id', i)
+    card.addEventListener('click', flipCard)
+    boardCards.push(card)
     gridDisplay.append(card)
  } 
 }
 
 createBoard()
+
+function checkMatch() {
+  if (cardChosen.size === 1) {
+    alert('Right choose!')
+    cardChosenIds.forEach( function(value){
+      boardCards[value].removeEventListener('click', flipCard)
+    })
+  } else {
+    cardChosenIds.forEach( function(value){
+      boardCards[value].setAttribute('src', 'images/blank.png')
+    })
+  }
+  cardChosen.clear();
+  cardChosenIds.clear();
+}
+
+function flipCard() {
+  const cardId = this.getAttribute('data-id')
+  cardChosenIds.add(cardId)
+  cardChosen.add(cardArray[cardId].name)
+  this.setAttribute('src', cardArray[cardId].img)
+  if (cardChosenIds.size === 2) {
+    setTimeout(checkMatch, 500)
+  }
+}
